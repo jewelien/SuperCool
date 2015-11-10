b0bf392793637a7f5490ea6ce801c8b6c12d617a
Julien Guanzon <julienguanzon@gmail.com>
2015-11-10 16:15:02 -0700

    auto layout

---
+++ SuperCool/AutoLayout.swift
@@ -0,0 +1,22 @@
+//
+//  AutoLayout.swift
+//  SuperCool
+//
+//  Created by Julien Guanzon on 11/10/15.
+//  Copyright © 2015 Julien Guanzon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AutoLayout: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
